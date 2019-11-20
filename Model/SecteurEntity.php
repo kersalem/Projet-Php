ce552d7a780297e6309b891f53e5b535464ca7c5
Paulien <paulien.m.73@gmail.com>
2019-11-20 15:40:22 +0100

    Create entity for secteur and structure

--- Model/SecteurEntity.php
+++ Model/SecteurEntity.php
@@ -6,5 +6,77 @@ namespace App\Entity;
 
 class SecteurEntity
 {
+    /**
+     * @var int $id
+     */
+    private $id;
 
+    /**
+     * @var string $libelle
+     */
+    private $libelle;
+
+    /**
+     * @var array $structures
+     */
+    private $structures;
+
+    /**
+     * SecteurEntity constructor.
+     * @param string $libelle
+     * @param array $structures
+     */
+    public function __construct($libelle, array $structures)
+    {
+        $this->libelle = $libelle;
+        $this->structures = $structures;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @param string $libelle
+     */
+    public function setLibelle($libelle)
+    {
+        $this->libelle = $libelle;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStructures()
+    {
+        return $this->structures;
+    }
+
+    /**
+     * @param array $structures
+     */
+    public function setStructures($structures)
+    {
+        $this->structures = $structures;
+    }
+
+    /**
+     * @param StructureEntity $structure
+     */
+    public function addStructure($structure)
+    {
+        $this->structures[] = $structure;
+    }
 }
---
+++ Model/StructureEntity.php
@@ -0,0 +1,221 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class StructureEntity
+{
+    /**
+     * @var int $id
+     */
+    private $id;
+
+    /**
+     * @var string $nom
+     */
+    private $nom;
+
+    /**
+     * @var string $rue
+     */
+    private $rue;
+
+    /**
+     * @var string $cp
+     */
+    private $cp;
+
+    /**
+     * @var string $ville
+     */
+    private $ville;
+
+    /**
+     * @var boolean $estAsso
+     */
+    private $estAsso;
+
+    /**
+     * @var int $nbDonateurs
+     */
+    private $nbDonateurs;
+
+    /**
+     * @var int $nbActionnaires
+     */
+    private $nbActionnaires;
+
+    /**
+     * @var array $secteurs
+     */
+    private $secteurs;
+
+    /**
+     * StructureEntity constructor.
+     * @param string $nom
+     * @param string $rue
+     * @param string $cp
+     * @param string $ville
+     * @param bool $estAsso
+     * @param int $nbDonateurs
+     * @param int $nbActionnaires
+     * @param array $secteurs
+     */
+    public function __construct($nom, $rue, $cp, $ville, $estAsso, $nbDonateurs, $nbActionnaires, $secteurs)
+    {
+        $this->nom = $nom;
+        $this->rue = $rue;
+        $this->cp = $cp;
+        $this->ville = $ville;
+        $this->estAsso = $estAsso;
+        $this->nbDonateurs = $nbDonateurs;
+        $this->nbActionnaires = $nbActionnaires;
+        $this->secteurs = $secteurs;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param string $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRue()
+    {
+        return $this->rue;
+    }
+
+    /**
+     * @param string $rue
+     */
+    public function setRue($rue)
+    {
+        $this->rue = $rue;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * @param string $cp
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * @param string $ville
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEstAsso()
+    {
+        return $this->estAsso;
+    }
+
+    /**
+     * @param bool $estAsso
+     */
+    public function setEstAsso($estAsso)
+    {
+        $this->estAsso = $estAsso;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbDonateurs()
+    {
+        return $this->nbDonateurs;
+    }
+
+    /**
+     * @param int $nbDonateurs
+     */
+    public function setNbDonateurs($nbDonateurs)
+    {
+        $this->nbDonateurs = $nbDonateurs;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbActionnaires()
+    {
+        return $this->nbActionnaires;
+    }
+
+    /**
+     * @param int $nbActionnaires
+     */
+    public function setNbActionnaires($nbActionnaires)
+    {
+        $this->nbActionnaires = $nbActionnaires;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSecteurs()
+    {
+        return $this->secteurs;
+    }
+
+    /**
+     * @param array $secteurs
+     */
+    public function setSecteurs($secteurs)
+    {
+        $this->secteurs = $secteurs;
+    }
+
+    /**
+     * @param SecteurEntity $secteur
+     */
+    public function addSecteur($secteur)
+    {
+        $this->secteurs[] = $secteur;
+    }
+
+}
