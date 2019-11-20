4f1580d1115f054e56c0aa3fdaa945474ab23a83
Paulien <paulien.m.73@gmail.com>
2019-11-20 14:56:11 +0100

    Create some route in AdminController

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -6,5 +6,52 @@ namespace App\Controller;
 
 class AdminController
 {
+    /**
+     * @param array $route
+     */
+    public function get(array $route)
+    {
+        switch ($route[2]) {
+            case "edit":
+                $this->editAction($route);
+                break;
+            case "create":
+                $this->createAction();
+                break;
+            case "delete":
+                $this->deleteAction($route);
+                break;
+            case "list":
+                $this->listAction();
+                break;
+            default:
+                $this->error404Action();
+                break;
+        }
+    }
 
+    public function editAction(array $route)
+    {
+
+    }
+
+    public function createAction()
+    {
+
+    }
+
+    public function deleteAction(array $route)
+    {
+
+    }
+
+    public function listAction()
+    {
+
+    }
+
+    public function error404Action()
+    {
+
+    }
 }
