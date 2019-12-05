90ad3e2b7408d2d862134a331badf14272c05eb0
kersalem <kersalemarie@outlook.com>
2019-12-05 09:48:15 +0100

    creer manager pour secteur

---
+++ Model/Manager/SecteurManager.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace mvc\model\manager;
+
+require_once(__DIR__ . '/../entities/Secteur.php');
+require_once('PDOManager.php');
+
+use App\Entity\Entity;
+use App\Entity\Secteur;
+use \PDOStatement;
+
+class SecteurManager extends PDOManager
+{
+    /**
+     * @param int $id
+     * @return Entity|null
+     */
+    public function findById(int $id): ?Entity
+    {
+        $stmt = $this->executePrepare("select * from secteur where id=:id", ["id" => $id]);
+        $secteur = $stmt->fetch();
+        if (!$secteur) return null;
+        return new Secteur($secteur["ID"], $secteur["LIBELLE"]);
+    }
+
+    public function find(): PDOStatement
+    {
+        $stmt = $this->executePrepare("select * from secteur", []);
+        return $stmt;
+    }
+
+    public function findAll(int $pdoFecthMode): array
+    {
+        $stmt = $this->find();
+        $secteurs = $stmt->fetchAll($pdoFecthMode);
+
+        $secteursEntities = [];
+        foreach ($secteurs as $secteur) {
+            $secteursEntities[] = new Secteur($secteur["ID"], $secteur["LIBELLE"]);
+        }
+        return $secteursEntities;
+    }
+
+    /**
+     * @param Entity $e
+     * @return PDOStatement
+     */
+    public function insert(Entity $e): PDOStatement
+    {
+        $req = "insert into secteur(id, libelle) values (:id, :libelle)";
+        $params = array("id" => $e->getId(), "libelle" => $e->getLibelle());
+        $res = $this->executePrepare($req, $params);
+        return $res;
+    }
+}
