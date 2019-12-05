b1e1bab914cf9e3f2a89803a1550682ed5ce6deb
Paulien <paulien.m.73@gmail.com>
2019-12-05 09:15:10 +0100

    Adapt listStructures and reformat

--- View/Admin/listStructures.php
+++ View/Admin/listStructures.php
@@ -1,31 +1,47 @@
 <?php
+
 $title = "Les structures";
-include ('View/base.php');
-require_once ('Model/Entity/Structure.php');
+include('View/base.php');
+require_once('Model/Entity/Structure.php');
+require_once('Model/Entity/Association.php');
+require_once('Model/Entity/Entreprise.php');
 ?>
 
 <body>
 <div class="container">
     <h1 class="text-center mt-4">Les structures</h1>
-    <a href="/admin/structure/create" class="btn btn-success"><span class="fa fa-plus mr-2 "></span>Créer une structure</a>
+    <a href="/admin/structure/create" class="btn btn-success">
+        <span class="fa fa-plus mr-2 "></span>
+        Créer une structure
+    </a>
     <div class="row">
         <?php foreach ($structures as $structure) { ?>
-        <div class="col-md-4 mt-4">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title"><?= $structure->getNom() ?></h4>
-                    <h6 class="card-subtitle mb-2 text-muted"><?= $structure->getRue(). ", " .$structure->getCp(). ", " .$structure->getVille() ?></h6>
-                    <p class="card-text">Nombre de donateurs : <?= $structure->getNbDonateurs()?></p>
-                    <p class="card-text">Nomnbre d'actionnaires : <?= $structure->getNbActionnaires()?></p>
-                    <p class="card-text">Secteur(s) d'activité : <?= implode(", ", $structure->getSecteurs()) ?></p>
-                    <a href="/admin/structure/edit" class="btn btn-primary">Modifier</a>
-                    <a href="/admin/structure/delete" class="btn btn-danger">Supprimer</a>
-                </div>
-                <div class="card-footer text-muted">
-                    <?= $structure->isEstAsso() ? "Association" : "Entreprise" ?>
+            <div class="col-md-4 mt-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title"><?= $structure->getNom() ?></h4>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            <?=
+                            $structure->getRue() . ", " . $structure->getCp() . ", " . $structure->getVille()
+                            ?>
+                        </h6>
+                        <p class="card-text">
+                            <?= $structure instanceof \App\Entity\Association
+                                ? "Nombre de donateurs : "
+                                  . $structure->getNbDonateurs()
+                                : "Nomnbre d'actionnaires : "
+                                  . $structure->getNbActionnaires() ?>
+                        </p>
+                        <p class="card-text">Secteur(s) d'activité : <?= implode(", ", $structure->getSecteurs()) ?></p>
+                        <a href="/admin/structure/edit" class="btn btn-primary">Modifier</a>
+                        <a href="/admin/structure/delete"
+                           class="btn btn-danger">Supprimer</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <?= $structure->isEstAsso() ? "Association" : "Entreprise" ?>
+                    </div>
                 </div>
             </div>
-        </div>
         <?php } ?>
     </div>
 
