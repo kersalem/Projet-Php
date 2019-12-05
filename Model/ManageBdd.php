c4d24ecfb50a7f8ec682c4214541cef8bc07d3c4
Paulien <paulien.m.73@gmail.com>
2019-12-05 08:29:03 +0100

    Create PDOManager.php to communicate with BDD

--- Model/ManageBdd.php
+++ Model/ManageBdd.php
@@ -1,15 +1,27 @@
 <?php
 
-define( 'DB_NAME', 'db_projetPhp' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'root' );
-define( 'DB_HOST', 'localhost' );
+define('DB_NAME', 'db_projetPhp');
+define('DB_USER', 'pama');
+define('DB_PASSWORD', 'pama');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
 // connexion à Mysql sans base de données
-$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.'charset=utf8', DB_USER, DB_PASSWORD);
+//$dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME;
+//var_dump($dsn);
+try {
+    $pdo = new PDO(
+        'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME
+        . ';charset=utf8', DB_USER, DB_PASSWORD
+    );
+} catch (Exception $e) {
+    die("Error : " . $e->getMessage());
+}
 
 // création de la requête sql
 // on teste avant si elle existe ou non (par sécurité)
 $requete = "SELECT * From secteur";
 
 // on prépare et on exécute la requête
-$test = $pdo->prepare($requete)->execute();
+$test = $pdo->prepare($requete);
+$test->execute();
+var_dump($test->fetchAll());
---
+++ Model/Manager/PDOManager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace mvc\model\manager;
+
+require_once(__DIR__ . '/../../conf/config.php');
+
+use mvc\model\entities\Entity;
+use \PDO;
+use \PDOStatement;
+use \PDOException;
+
+abstract class PDOManager
+{
+    /*private string $host, $db, $encoding, $user, $pass;
+    private int $pdoErrorMode;*/
+    private $host, $db, $encoding, $user, $pass;
+    private $pdoErrorMode;
+
+    /**
+     * Manager constructor
+     */
+    public function __construct()
+    {
+        $this->host         = $GLOBALS["host"];
+        $this->db           = $GLOBALS["db"];
+        $this->encoding     = $GLOBALS["encoding"];
+        $this->user         = $GLOBALS["user"];
+        $this->pass         = $GLOBALS["pass"];
+        $this->pdoErrorMode = $GLOBALS["pdoErrorMode"];
+    }
+
+    protected function dbConnect(): PDO
+    {
+        $conn = new PDO(
+            "mysql:host=$this->host;dbname=$this->db;charset=$this->encoding",
+            $this->user,
+            $this->pass
+        );
+        $conn->setAttribute(PDO::ATTR_ERRMODE, $this->pdoErrorMode);
+
+        return $conn;
+    }
+
+    protected function executePrepare(string $req, array $params): PDOStatement
+    {
+        $conn = null;
+        try {
+            $conn = $this->dbConnect();
+            $stmt = $conn->prepare($req);
+            $res  = $stmt->execute($params);
+
+            return $stmt;
+        } catch (PDOException $ex) {
+            throw $ex;
+        } finally {
+            if ($conn != null) {
+                $conn = null;
+            }
+        }
+    }
+
+    public abstract function findById(int $id): ?Entity;
+
+    public abstract function find(): PDOStatement;
+
+    public abstract function findAll(int $pdoFecthMode): array;
+
+    public abstract function insert(Entity $e): PDOStatement;
+}
