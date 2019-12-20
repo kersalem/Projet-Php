dced9bad1f3e820603262d5fe67b151a097e7628
Paulien <paulien.m.73@gmail.com>
2019-12-20 16:16:06 +0100

    Use router to generate link in navbar

--- View/navBar.php
+++ View/navBar.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Consulting app</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <a class="navbar-brand" href="<?= $router->path('default.index') ?>">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/admin/secteur">Secteurs</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/admin/structure">Structures</a>
-      </li>
-    </ul>
-  </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $router->path('secteur.list') ?>">Secteurs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= $router->path('structure.list') ?>">Structures</a>
+            </li>
+        </ul>
+    </div>
 </nav>
