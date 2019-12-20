a7b7194605c522d6c05129f91a709edd54598feb
Paulien <paulien.m.73@gmail.com>
2019-12-20 16:14:32 +0100

    Create homepage

---
+++ View/Default/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$headTitle = "Accueil";
+include('View/head.php');
+?>
+<body>
+<?php include('View/navBar.php'); ?>
+<div class="container">
+
+</div>
+</body>
+</html>
