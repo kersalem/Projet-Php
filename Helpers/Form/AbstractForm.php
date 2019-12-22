a9a37304fd40be17e23aa46ff79a8dfdb0c5b775
Paulien <paulien.m.73@gmail.com>
2019-12-22 21:13:05 +0100

    Create AbstractForm

---
+++ Helpers/Form/AbstractForm.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Helpers\Form;
+
+
+use App\Entity\Entity;
+
+abstract class AbstractForm
+{
+    abstract public function formIsValid(): bool;
+
+    abstract public function handleForm(Entity $e);
+
+    abstract public function getFormValues($structure): array;
+
+    public function setPostValuesInSession() {
+        session_start();
+        foreach ($_POST as $key => $value) {
+            $_SESSION[$key] = $value;
+        }
+    }
+}
